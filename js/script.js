66506b8ba994c3006740644964de10a356dd4754
RenataBP <renatabossepenteado@gmail.com>
2024-12-05 10:02:21 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const mario = document.querySelector('.mario')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
       mario.classList.add('jump');
@@ -7,5 +8,15 @@ setTimeout(() => {  mario.classList.remove('jump');}, 500);
 
 }
 
+const loop = setInterval(() => {
+      const pipePosition = pipe.offsetLeft;
+      const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
 
+      console.log(marioPosition);
+
+      if (pipePosition <= 120 && marioPosition < 100) {
+            pipe.style.animation = 'none'; // corrigido para parar a animação
+            pipe.style.left = `${pipePosition}px`; //Mantém a posição do pipe
+      }
+      }, 10);
 window.document.addEventListener('keydown', jump);
